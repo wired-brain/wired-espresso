444c5e57b02783194e4ea85013b19decce17e917
Kamran Ayub <kamran.ayub@gmail.com>
2019-11-30 22:46:42 -0600

    chore(PID): Add initial tests (#12)

---
+++ src/__tests__/PID.test.ts
@@ -0,0 +1,39 @@
+import { applyRequestedTemperature, TemperatureContext } from "../PID";
+import { DeviceMakeModel } from "../Devices";
+
+let mockMakeAndModel: DeviceMakeModel;
+let mockVersion: number;
+
+jest.mock("../DeviceContext", () => ({
+  DeviceContext: {
+    get makeAndModel() {
+      return mockMakeAndModel;
+    },
+    get version() {
+      return mockVersion;
+    }
+  }
+}));
+
+describe("applyRequestedTemperature", () => {
+  beforeEach(() => {
+    // default make & model
+    mockMakeAndModel = DeviceMakeModel.ECM_SYNCHRONIKA;
+    mockVersion = 1;
+  });
+
+  it("should workaround temperature issue in LUCCA M58 v2 models", () => {
+    mockMakeAndModel = DeviceMakeModel.LUCCA_M58;
+    mockVersion = 2;
+
+    applyRequestedTemperature(205);
+
+    expect(TemperatureContext.requestedTemperature).toBe(204);
+  });
+
+  it("should set temp context appropriately", () => {
+    applyRequestedTemperature(205);
+
+    expect(TemperatureContext.requestedTemperature).toBe(205);
+  });
+});
