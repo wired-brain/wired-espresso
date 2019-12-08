b0091f79a2b5c24b4a5bfa442e092e5c454fda40
Kamran Ayub <kamran.ayub@gmail.com>
2019-12-07 22:17:28 -0600

    fix(Reservoir): Warn when water is above threshold

--- src/Reservoir.ts
+++ src/Reservoir.ts
@@ -1,13 +1,20 @@
+import { WATER_ABOVE_THRESHOLD_ERROR } from "./constants";
+
+const MAX_WATER_THRESHOLD = 100;
+
 let waterLevel: number = 0;
 
 export function getWaterLevel() {
-    return waterLevel
+  return waterLevel;
 }
 
 export function setWaterLevel(value: number) {
-    waterLevel = value;
+  waterLevel = value;
 }
 
 export function validate(): boolean {
-    return waterLevel >= 0;
+  if (waterLevel > MAX_WATER_THRESHOLD) {
+    throw new Error(`Code: ${WATER_ABOVE_THRESHOLD_ERROR}`);
+  }
+  return waterLevel >= 0;
 }
--- src/__tests__/Reservoir.test.ts
+++ src/__tests__/Reservoir.test.ts
@@ -1,8 +1,16 @@
-import { getWaterLevel, setWaterLevel } from "../Reservoir";
+import { getWaterLevel, setWaterLevel, validate } from "../Reservoir";
+import { WATER_ABOVE_THRESHOLD_ERROR } from "../constants";
 
 describe("Reservoir", () => {
   it("should set water level to value", () => {
     setWaterLevel(60);
     expect(getWaterLevel()).toBe(60);
+  });
+
+  it(`should throw error code ${WATER_ABOVE_THRESHOLD_ERROR} if above threshold when validated`, () => {
+    setWaterLevel(101);
+    expect(() => validate()).toThrowError(
+      `Code: ${WATER_ABOVE_THRESHOLD_ERROR}`
+    );
   });
 });
