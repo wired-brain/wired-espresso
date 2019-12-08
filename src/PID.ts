d259c1829bb60d0682d6fef0cdfb9d1aea06a1c7
Kamran Ayub <kamran.ayub@gmail.com>
2019-12-08 14:40:13 -0600

    fix(PID): Only apply temperature workaround to LUCCA M58 v2 models (#9)
    ## Changes
    
    - Modified switch statement in PID controller to only apply temperature change for V2 models

--- src/PID.ts
+++ src/PID.ts
@@ -6,12 +6,17 @@ export const TemperatureContext = {
 }
 
 export function applyRequestedTemperature(temp: number) {
-  const { makeAndModel } = DeviceContext;
+  const { makeAndModel, version } = DeviceContext;
 
   switch (makeAndModel) {
     case DeviceMakeModel.LUCCA_M58:
-      TemperatureContext.requestedTemp = temp - 1;
-      break;
+
+      // See issue: https://github.com/wired-brain/wired-espresso/issues/3
+      // LUCCA M58 V2 has a bad hardware temperature report
+      if (version === 2) {
+        TemperatureContext.requestedTemp = temp - 1;
+        break;
+      }
     default:
       TemperatureContext.requestedTemp = temp;
       break;
--- src/__tests__/PID.test.ts
+++ src/__tests__/PID.test.ts
@@ -31,6 +31,15 @@ describe("applyRequestedTemperature", () => {
     expect(TemperatureContext.requestedTemp).toBe(204);
   });
 
+  it("should not workaround temperature issue in LUCCA M58 v3 models", () => {
+    mockMakeAndModel = DeviceMakeModel.LUCCA_M58;
+    mockVersion = 3;
+
+    applyRequestedTemperature(205);
+
+    expect(TemperatureContext.requestedTemp).toBe(205);
+  });
+
   it("should set temp context appropriately", () => {
     applyRequestedTemperature(205);
 
