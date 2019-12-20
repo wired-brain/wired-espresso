ac95be10b9e0840fdce400f0631a6a48f39bc022
Kamran Ayub <kamran.ayub@gmail.com>
2019-12-19 23:00:48 -0600

    fix(Reservoir): Add warning when water is above threshold (#17)
    ## Changes
    
    - Adds a new constant error code
    - Adds a check for the water threshold
    - Adds tests

--- src/__tests__/Reservoir.test.ts
+++ src/__tests__/Reservoir.test.ts
@@ -1,8 +1,24 @@
-import { getWaterLevel, setWaterLevel } from "../Reservoir";
+import { getWaterLevel, setWaterLevel, validate } from "../Reservoir";
+import { WATER_ABOVE_THRESHOLD_ERROR } from "../constants";
 
 describe("Reservoir", () => {
   it("should set water level to value", () => {
     setWaterLevel(60);
     expect(getWaterLevel()).toBe(60);
   });
+
+  it(`should throw error code ${WATER_ABOVE_THRESHOLD_ERROR} if above threshold when validated`, () => {
+    setWaterLevel(101);
+    expect(() => validate()).toThrowError(
+      `Code: ${WATER_ABOVE_THRESHOLD_ERROR}`
+    );
+  });
+  
+  it('should validate water is 0 or above', () => {
+    setWaterLevel(0)
+    expect(validate()).toBe(true);
+
+    setWaterLevel(-1)
+    expect(validate()).toBe(false);
+  })
 });
